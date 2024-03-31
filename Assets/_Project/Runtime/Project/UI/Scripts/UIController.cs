b9fbe94857e5d9ce2162db15140aa5ce982f24c9
Ömer Abuzer Sarıkaş <omer.97.63.gs@gmail.com>
2024-03-31 18:40:13 +0300

    fix

--- Assets/_Project/Runtime/Project/UI/Scripts/UIController.cs
+++ Assets/_Project/Runtime/Project/UI/Scripts/UIController.cs
@@ -7,12 +7,16 @@ using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    public GameObject Scene1,Scene2,PotionItemSelected;
     public CanvasGroup Scene1Canvas,Scene2Canvas;
-    public RectTransform Scene1Header,Scene1Footer,Scene1BodyRight,Scene1BodyLeft,Scene2Header,Scene2Body,Scene2Footer;
+    public RectTransform Scene1Header,Scene1Footer,Scene1BodyRight,Scene1BodyLeft,Scene2Header,Scene2Body,
+    Scene2Footer,PotionItemSelectedRect;
+
     void Awake(){
         Scene1Canvas.alpha=1;
         Scene2Canvas.alpha=0;
+        PotionItemSelected.active=false;
     }
     void Start()
     {
@@ -20,25 +24,23 @@ public class UIController : MonoBehaviour
         Scene2Body.DOAnchorPosY(Scene2Body.anchoredPosition.y+10, 0.1f);
         Scene2Footer.DOAnchorPosY(Scene2Header.anchoredPosition.y-10, 0.1f);
     }
-
-    // Update is called once per frame
     void Update()
     {
         
     }
-
     public void InventoryClick(){
         Scene1Header.DOAnchorPosY(10, 0.5f);
         Scene1Footer.DOAnchorPosY(-10, 0.5f);
         Scene1BodyRight.DOAnchorPosX(10, 0.5f);
         Scene1BodyLeft.DOAnchorPosX(-10, 0.5f);
-        DOTween.To(()=>Scene1Canvas.alpha,x=>Scene1Canvas.alpha=x,0,1);
+        DOTween.To(()=>Scene1Canvas.alpha,x=>Scene1Canvas.alpha=x,0,1).OnComplete(()=>{Scene1.active=false;});
         Scene2Header.DOAnchorPosY(0, 0.5f);
         Scene2Body.DOAnchorPosY(0, 0.5f);
         Scene2Footer.DOAnchorPosY(0, 0.5f);
         DOTween.To(()=>Scene2Canvas.alpha,x=>Scene2Canvas.alpha=x,1,1);
-
-        //Scene1Header.DOAnchorPosY(Scene1Header.offsetMax.y,Scene1Header.offsetMax.y-100).SetRelative(true);
-        
+    }
+    public void ItemPotionClick(){
+        PotionItemSelected.active=true;
+        PotionItemSelectedRect.DOScale(1.1f, 1f).SetLoops(5,LoopType.Yoyo);
     }
 }
